c560e0c653817ddff196d73bd3db362a1df7b939
Piotr Galar <fjalar.galar@gmail.com>
2015-04-30 19:08:27 +0200

    First commit

---
+++ Pavo/Pavo.h
@@ -0,0 +1,17 @@
+//
+//  Pavo.h
+//  Pavo
+//
+//  Created by Piotr Galar on 30/04/2015.
+//  Copyright (c) 2015 Piotr Galar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pavo.
+FOUNDATION_EXPORT double PavoVersionNumber;
+
+//! Project version string for Pavo.
+FOUNDATION_EXPORT const unsigned char PavoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pavo/PublicHeader.h>
